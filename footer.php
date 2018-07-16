7610c6494d2937a4e3de0460d37dd7a05af32134
Julio Lovera <lekitar123@gmail.com>
2018-07-16 15:53:20 -0400

    Carga inicial

---
+++ footer.php
@@ -0,0 +1,9 @@
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript" ></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>
+
+
+
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Luxury </title>
+</head>
+<link rel="stylesheet" href="css/main.css">
+
+
+<body>
