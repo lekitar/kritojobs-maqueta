7543614b87e59a61109d7a728d6c10b590d62112
Julio Lovera <lekitar123@gmail.com>
2018-08-04 18:12:33 -0400

    Plantillas nuevas

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 <?php include('p-inicio/banner.php');?>
 <?php include('p-inicio/buscador.php');?>
 <?php include('p-inicio/destacados.php'); ?>
+<?php include('p-inicio/category-destacadas.php'); ?>
+<?php include('p-inicio/usuarios-destacadas.php'); ?>
 <?php include('footer.php');?>
--- p-inicio/destacados.php
+++ p-inicio/destacados.php
@@ -1,9 +1,8 @@
 <div class="container-fluid" id="destacados">
 	<div class="row">
 		<div class="container"> 
-
 			<div class="row"> 
-				<div class="col-md-12 text-center"> 
+				<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12 text-center"> 
 					<h2>Trabajos Recientes</h2>
 					<h4>Empleadores líderes que ya utilizan el trabajo y el talento.</h4>
 				</div>
@@ -13,9 +12,11 @@
 							<div class="c-logo "> <img  class="img-responsive center-block" src="images/logokriptojobs.png" alt=""> </div>
 						<h3> Diseñador Grafico (UI/UX)</h3>
 						<a href="#1">KriptoJobs</a>
+						<span> <i class="fas fa-wallet texto-verde"> </i> 500 - 1500 sbd</span>
 						</div>
 						<div class="job-lctn">
-							Caracas, Venezuela
+							<span> Caracas, Venezuela </span>
+							
 						</div>
 						<div class="popular ">
 							<p> Destacado</p>
@@ -31,9 +32,11 @@
 							<div class="c-logo "> <img  class="img-responsive center-block" src="images/logokriptojobs.png" alt=""> </div>
 						<h3> Diseñador Grafico (UI/UX)</h3>
 						<a href="#2">KriptoJobs</a>
+						<span> <i class="fas fa-wallet texto-verde"> </i> 500 - 1500 sbd</span>
 						</div>
 						<div class="job-lctn">
-							Caracas, Venezuela
+							<span> Caracas, Venezuela </span>
+							
 						</div>
 						<button class="btn-default btn btn-verde">Apply Now</button>
 					</div>
@@ -44,9 +47,11 @@
 							<div class="c-logo "> <img  class="img-responsive center-block" src="images/logokriptojobs.png" alt=""> </div>
 						<h3> Diseñador Grafico (UI/UX)</h3>
 						<a href="#3">KriptoJobs</a>
+						<span> <i class="fas fa-wallet texto-verde"> </i> 500 - 1500 sbd</span>
 						</div>
 						<div class="job-lctn">
-							Caracas, Venezuela
+							<span> Caracas, Venezuela </span>
+							
 						</div>
 						<button class="btn-default btn btn-verde">Apply Now</button>
 					</div>
@@ -57,9 +62,11 @@
 							<div class="c-logo "> <img  class="img-responsive center-block" src="images/logokriptojobs.png" alt=""> </div>
 						<h3> Diseñador Grafico (UI/UX)</h3>
 						<a href="#4">KriptoJobs</a>
+						<span> <i class="fas fa-wallet texto-verde"> </i> 500 - 1500 sbd</span>
 						</div>
 						<div class="job-lctn">
-							Caracas, Venezuela
+							<span> Caracas, Venezuela </span>
+							
 						</div>
 						<button class="btn-default btn btn-verde">Apply Now</button>
 					</div>
@@ -70,9 +77,11 @@
 							<div class="c-logo "> <img  class="img-responsive center-block" src="images/logokriptojobs.png" alt=""> </div>
 						<h3> Diseñador Grafico (UI/UX)</h3>
 						<a href="#5">KriptoJobs</a>
+						<span> <i class="fas fa-wallet texto-verde"> </i> 500 - 1500 sbd</span>
 						</div>
 						<div class="job-lctn">
-							Caracas, Venezuela
+							<span> Caracas, Venezuela </span>
+							
 						</div>
 						<button class="btn-default btn btn-verde">Apply Now</button>
 					</div>
@@ -83,9 +92,11 @@
 							<div class="c-logo "> <img  class="img-responsive center-block" src="images/logokriptojobs.png" alt=""> </div>
 						<h3> Diseñador Grafico (UI/UX)</h3>
 						<a href="#">KriptoJobs</a>
+						<span> <i class="fas fa-wallet texto-verde"> </i> 500 - 1500 sbd</span>
 						</div>
 						<div class="job-lctn">
-							Caracas, Venezuela
+							<span> Caracas, Venezuela </span>
+							
 						</div>
 						<button class="btn-default btn btn-verde">Apply Now</button>
 					</div>
